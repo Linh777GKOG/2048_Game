db6e25179d511aaa545974763aef59f97140dced
Linh Vu <linh777gkog@gmail.com>
2022-06-17 22:48:06 +0700

    check point

---
+++ Grid.js
@@ -0,0 +1,49 @@
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
+
+export default class Grid {
+  #cells;
+
+  constructor(gridElement) {
+    gridElement.style.setProperty('--grid-size', GRID_SIZE);
+    gridElement.style.setProperty('--cell-size', `${CELL_SIZE}vmin`);
+    gridElement.style.setProperty('--cell-gap', `${CELL_GAP}vmin`);
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      );
+    });
+  }
+
+  get cells() {
+    return this.#cells;
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get #emptyCells() {
+    return this.#cells.filter((cell) => cell.tile == null);
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+    return this.#emptyCells[randomIndex];
+  }
+}
