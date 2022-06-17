e93eb328fc39ea94cb5d31e9c5859b6b37c6dfff
Linh Vu <linh777gkog@gmail.com>
2022-06-17 22:45:19 +0700

    check point

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ setupInput();
 function setupInput() {
   window.addEventListener('keydown', handleInput, { once: true });
 }
+
+async function handleInput(e) {
+  switch (e.key) {
+    case "ArrowUp":
+      if (!canMoveUp()) {
+        setupInput()
+        return
+      }
+      await moveUp()
+      break
+    case "ArrowDown":
+      if (!canMoveDown()) {
+        setupInput()
+        return
+      }
+      await moveDown()
+      break
+    case "ArrowLeft":
+      if (!canMoveLeft()) {
+        setupInput()
+        return
+      }
+      await moveLeft()
+      break
+    case "ArrowRight":
+      if (!canMoveRight()) {
+        setupInput()
+        return
+      }
+      await moveRight()
+      break
+    default:
+      setupInput()
+      return
+  }
