7b6960b347fda82621021d1c9e42c16dba8dc4a2
Linh Vu <linh777gkog@gmail.com>
2022-06-17 22:46:12 +0700

    valid cell

--- script.js
+++ script.js
@@ -61,3 +61,48 @@ async function handleInput(e) {
 
   setupInput();
 }
+
+function moveUp() {
+  return slideTiles(grid.cellsByColumn);
+}
+
+function moveDown() {
+  return slideTiles(grid.cellsByColumn.map((column) => [...column].reverse()));
+}
+
+function moveLeft() {
+  return slideTiles(grid.cellsByRow);
+}
+
+function moveRight() {
+  return slideTiles(grid.cellsByRow.map((row) => [...row].reverse()));
+}
+
+function slideTiles(cells) {
+  return Promise.all(
+    cells.flatMap((group) => {
+      const promises = [];
+      for (let i = 1; i < group.length; i++) {
+        const cell = group[i];
+        if (cell.tile == null) continue;
+        let lastValidCell;
+        for (let j = i - 1; j >= 0; j--) {
+          const moveToCell = group[j];
+          if (!moveToCell.canAccept(cell.tile)) break;
+          lastValidCell = moveToCell;
+        }
+
+        if (lastValidCell != null) {
+          promises.push(cell.tile.waitForTransition());
+          if (lastValidCell.tile != null) {
+            lastValidCell.mergeTile = cell.tile;
+          } else {
+            lastValidCell.tile = cell.tile;
+          }
+          cell.tile = null;
+        }
+      }
+      return promises;
+    })
+  );
+}
